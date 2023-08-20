3ff01d05352b962be0f02eca30ca450870245504
shirrashko <srashkovits@lightricks.com>
2023-08-20 10:28:28 +0300

    wip

---
+++ pkg/api/model/model.go
@@ -0,0 +1,9 @@
+package model
+
+type UserProfile struct {
+	ID            int
+	Username      string `json:"username"` // unique per user
+	FullName      string `json:"full_name"`
+	Bio           string `json:"bio"`
+	ProfilePicURL string `json:"profile_pic_url"`
+}
